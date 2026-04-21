7529a5753eb58c44af7c1c59f070e5d81455298c
Basnat ALBASHITI <Basnat.ALBASHITI@student.hel.be>
2026-04-21 10:49:00 +0200

    Add project files.

---
+++ GestionProcessus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace GestionProcessus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p =new Process();
+
+            p.StartInfo.FileName = @"C:\Users\basan\OneDrive\Documents\Desktop\2em Q2\LABEYE\GestionProcessus\PgmSecondaire\bin\Debug\net8.0\PgmSecondaire.exe";
+            p.StartInfo.FileName = "notepad.exe";
+
+            //DateTime start = DateTime.Now;
+            p.Start();
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer Notepad...");
+            Console.ReadLine();
+
+            Console.WriteLine("HasExited = " + p.HasExited);
+
+            if (!p.HasExited)
+            {
+                p.Kill();
+                Console.WriteLine("Notepad fermé !");
+            }
+            else
+            {
+                Console.WriteLine("Notepad était déjà fermé.");
+
+            }
+        }
+    }
+}
---
+++ PgmSecondaire/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PgmSecondaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = Console.ReadLine();
+            string prenom = Console.ReadLine();
+
+            Console.WriteLine("Bonjour " + prenom + " " + nom);
+
+        }
+    }
+}
