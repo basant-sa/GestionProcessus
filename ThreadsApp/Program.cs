6b4e7151f388764d3edf6f58c6445f0e2593dd67
Basnat ALBASHITI <Basnat.ALBASHITI@student.hel.be>
2026-04-21 11:22:13 +0200

    a créé un nouveau projet appelé ThreadsApp et a écrit un code qui permet le mélange de F et G

---
+++ ThreadsApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ThreadsApp
+{
+    internal class Program
+    {
+      
+            const int N = 5;
+
+            static void f()
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write("FFFFFFFF ");
+                }
+            }
+
+            static void g()
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write("GGGGGGGG ");
+                }
+            }
+
+            static void Main()
+            {
+                Thread t1 = new Thread(f);
+                Thread t2 = new Thread(g);
+
+                t1.Start();
+                t2.Start();
+
+                t1.Join();
+                t2.Join();
+
+
+                Console.WriteLine("\nFin des threads");
+                Console.ReadLine();
+            }
+
+        
+    }
+}
